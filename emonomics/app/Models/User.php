8d56a1a02765cdfa6c52e0fde0fbb55cd7514545
Kong Mong Vang <vangkong@icloud.com>
2026-02-10 19:35:21 -0500

    register and login authentication

--- emonomics/app/Models/User.php
+++ emonomics/app/Models/User.php
@@ -10,9 +10,20 @@ class User extends Authenticatable
 {
     protected $table = 'users';
     protected $primaryKey = 'user_id';
-    public $timestamps = false;
+    public $timestamps = true;
 
-    protected $fillable = ['first_name', 'last_name', 'email', 'password'];
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
 
     public function admin(): HasOne
     {
